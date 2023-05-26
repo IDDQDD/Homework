1e1a7a63cc68f17d67e687ddd4b8e53b4b16d9ce
IDDQDD <39268229+IDDQDD@users.noreply.github.com>
2023-05-26 18:52:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 
 #zadacha 1
-import math
-def exp(a, b):
-    if b == 0 : return 1;
-    return a *exp(a, b - 1);
+'''chant1 = 'пара-ра-рам рам-пам-папам па-ра-па-да';
 
-print(exp(2, 3));
+def check_rythm(chant):
+    chant = chant.split();
+    count_vouwels = lambda x: sum(map(x.count, 'аоиыеуёэюя'));
+    if len(set(map(count_vouwels, chant))) == 1:
+        print('Парам пам-пам');
+    esle: print('Пам парам');
+
+check_rythm(chant1);'''
 
 #zadacha 2
-
-def sum(a, b):
-    if a == 0:
-        return b;
-    return sum(a - 1, b + 1);
-
-print(sum(2, 1));
+def operation_table(operation, num_columns=6, num_rows=6):
+    table = [[operation(i, j) for i in range(1, num_columns + 1)]
+                              for j in range(1, num_rows + 1)]
+    for i in table:
+        print(*i)
+operation_table(lambda x, y: x * y)
