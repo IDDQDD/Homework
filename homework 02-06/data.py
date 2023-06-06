b8ebd884959ff99b0bf24afda694f39f336288e3
IDDQDD <39268229+IDDQDD@users.noreply.github.com>
2023-06-06 19:45:51 +0300

    Update data.py

--- homework 02-06/data.py
+++ homework 02-06/data.py
@@ -1,4 +1,4 @@
 myrecords = {"Nikita : +37529333489", "Andrey : +375293434395",
-              "Olga : +37529343243", "Nastya : +37529092324"};
+              "Olga : +37529343243", "Nastya : +37529092324"}
 
-path = "phones.txt";
+path = "phones.txt"
