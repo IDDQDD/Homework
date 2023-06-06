a500c1de9fc1309a980a5ed10471107aa22ec19e
IDDQDD <39268229+IDDQDD@users.noreply.github.com>
2023-06-06 19:46:07 +0300

    Update main.py

--- homework 02-06/main.py
+++ homework 02-06/main.py
@@ -3,8 +3,8 @@ import phonebook
 
 
 if __name__ == "__main__":
-    pbook = phonebook.phonesbook(phonebook.data.myrecords);
-    pbook.add_contact('Danik','+3752939403493094');
-    pbook.delete_contact('Nikita');
-    pbook.change_phonenumber('Olga', '+3757777777');
-    del pbook;
+    pbook = phonebook.phonesbook(phonebook.data.myrecords)
+    pbook.add_contact('Danik','+3752939403493094')
+    pbook.delete_contact('Nikita')
+    pbook.change_phonenumber('Olga', '+3757777777')
+    del pbook
